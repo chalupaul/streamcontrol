1d698c6b54a8e7458a3877762d6e495e766c67f6
chalupaul <me@chalupaul.com>
2022-03-05 01:10:13 -0600

    black formatting

--- dodo.py
+++ dodo.py
@@ -1,16 +1,22 @@
-def _build_action(actions, verbosity = 2):
+code = "streamcontrol dodo.py"
+tests = "tests"
+
+
+def _build_action(actions, verbosity=2):
     return {"verbosity": verbosity, "actions": actions}
 
+
 def task_black():
-    return _build_action([f"black streamcontrol tests"])
+    return _build_action([f"black {code} {tests}"])
 
 
 def task_bandit():
-    return _build_action([f"bandit -r streamcontrol"])
+    return _build_action([f"bandit -r {code}"])
 
 
 def task_pyproject_lint():
     return _build_action(["poetry check"])
+
 
 def task_flake8():
     # line length of 88 to match black
@@ -21,10 +27,11 @@ def task_flake8():
                 " --max-line-length=88"
                 " --count"
                 " --statistics"
-                " streamcontrol tests"
+                f" {code} {tests}"
             )
         ]
     )
+
 
 def task_isort():
     return _build_action(["isort ."])
